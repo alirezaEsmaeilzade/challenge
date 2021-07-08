dd26d0b4450b39346c0d198f3891cee749a0e516
alirezaEsmaeilzade <69242428+alirezaEsmaeilzade@users.noreply.github.com>
2021-07-08 11:31:19 +0430

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
-x = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        x += i
+def S(r, x):
+    return ((x + (r//x)*x)/2) * (r//x)
+def sum_multiple(r, x, y):
+    r = r - 1
+    z = x * y
+    return int(S(r, x) + S(r, y) - S(r, z))
 
-print(x)
+print(sum_multiple(1000, 3, 5))
